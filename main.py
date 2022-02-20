b7aece0a3d20e42e4407c9f0ef8810838cf59ea6
JazsminDouglas <99223970+jazsmindouglas@users.noreply.github.com>
2022-02-20 18:20:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+print("Please type the first item on a shopping list.")
+itema = input()
+print("Please type the second item on a shopping list.")
+itemb = input()
+print("Please type the third item on a shopping list.")
+itemc = input()
+shopping_list = [itema,itemb,itemc]
+print(shopping_list)
